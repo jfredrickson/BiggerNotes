273e72c90fd65db1e0423fdca315e0b83c9997eb
Jeff Fredrickson <jeff@ejeff.org>
2023-08-07 16:48:05 -0600

    Revert "add configurable New Note button position"
    This reverts commit 771ecdba712b29023a97fbb9e8af348e8f0f6e49.

--- BiggerNotes/UI/AppSettingsSheet.swift
+++ BiggerNotes/UI/AppSettingsSheet.swift
@@ -30,12 +30,6 @@ struct AppSettingsSheet: View {
                     Toggle(isOn: $appSettingsViewModel.startWithNewNote, label: {
                         Text("Show blank note on start")
                     })
-
-                    Picker("New note button position", selection: $appSettingsViewModel.newNoteButtonPosition) {
-                        ForEach(NewNoteButtonPosition.allCases) { option in
-                            Text("\(option)".capitalized).tag(option)
-                        }
-                    }
                 }
                 
                 Section {
