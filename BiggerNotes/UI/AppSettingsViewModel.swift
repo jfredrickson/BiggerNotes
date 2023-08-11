586e0d4f0aec90e04c5a4f66b8f3cc6cd0bcd6a4
Jeff Fredrickson <jeff@ejeff.org>
2023-08-11 09:09:07 -0600

    add a floating new note button option

--- BiggerNotes/UI/AppSettingsViewModel.swift
+++ BiggerNotes/UI/AppSettingsViewModel.swift
@@ -9,12 +9,39 @@ import SwiftUI
 
 class AppSettingsViewModel: ObservableObject {
     static let DefaultStartWithNewNote = false
+    static let DefaultNewNoteButtonPosition = NewNoteButtonPosition.top
 
     @AppStorage("startWithNewNote") var startWithNewNote = DefaultStartWithNewNote
     @AppStorage("expandFavoritesSection") var expandFavoritesSection = true
     @AppStorage("expandNotesSection") var expandNotesSection = true
+    @AppStorage("newNoteButtonPosition") var newNoteButtonPosition = DefaultNewNoteButtonPosition
 
     func resetToDefaults() {
         startWithNewNote = AppSettingsViewModel.DefaultStartWithNewNote
+        newNoteButtonPosition = AppSettingsViewModel.DefaultNewNoteButtonPosition
     }
+}
+
+enum NewNoteButtonPosition: Int, CaseIterable, Identifiable {
+    case top
+    case bottom
+    case both
+
+    var text: String {
+        switch self {
+        case .top: return "Top"
+        case .bottom: return "Bottom"
+        case .both: return "Both"
+        }
+    }
+    
+    var includesTop: Bool {
+        return self == .top || self == .both
+    }
+    
+    var includesBottom: Bool {
+        return self == .bottom || self == .both
+    }
+    
+    var id: Self { self }
 }
