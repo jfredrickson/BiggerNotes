d20d829c46c900fe94a25e31bf2aa1810bcc3725
Jeff Fredrickson <jeff@ejeff.org>
2025-06-13 22:23:52 -0600

    use view modifiers on TextArea rather than accessing TextSettingsViewModel

--- BiggerNotes/UI/NoteDetail.swift
+++ BiggerNotes/UI/NoteDetail.swift
@@ -13,18 +13,19 @@ struct NoteDetail: View {
     @EnvironmentObject var noteViewModel: NoteViewModel
     @EnvironmentObject var textSettingsViewModel: TextSettingsViewModel
     @ObservedObject var note: Note
+    @FocusState var isEditing: Bool
 
     var body: some View {
         VStack {
             TextView(text: $note.content)
-            .onDisappear {
-                noteViewModel.save()
-            }
-            .background(textSettingsViewModel.useCustomColors ? textSettingsViewModel.backgroundColor.color : .clear)
+                .font(textSettingsViewModel.uiFont)
+                .textColor(textSettingsViewModel.uiTextColor)
+                .backgroundColor(textSettingsViewModel.uiBackgroundColor)
+                .onDisappear { noteViewModel.save() }
+                .focused($isEditing)
         }
-        .navigationBarTitleDisplayMode(.inline)
         .toolbar {
-            ToolbarItemGroup(placement: .navigationBarTrailing) {
+            ToolbarItemGroup(placement: .topBarTrailing) {
                 // Text Settings
                 SettingsButton(showingSheet: $router.showingTextSettings, title: "Text Settings", systemImage: "textformat.size") {
                     TextSettingsSheet()
